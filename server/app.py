1ef1ca4f22f8784cb04b0c9f79bc33654a4e1f58
masud5201~ <your.email@masud.abdi@student.moringaschool.com>
2024-02-02 13:44:24 -0800

    done

--- server/app.py
+++ server/app.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, jsonify
 from flask_migrate import Migrate
-
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
 migrate = Migrate(app, db)
 
@@ -19,31 +17,25 @@ def home():
     return '<h1>Bakery GET-POST-PATCH-DELETE API</h1>'
 
 @app.route('/bakeries')
-def bakeries():
+def get_all_bakeries():
     bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
-    return make_response(  bakeries,   200  )
+    return jsonify(bakeries)
 
 @app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-
-    bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
-    return make_response ( bakery_serialized, 200  )
+def get_bakery_by_id(id):
+    bakery = Bakery.query.get_or_404(id)
+    return jsonify(bakery.to_dict())
 
 @app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price.desc()).all()
-    baked_goods_by_price_serialized = [
-        bg.to_dict() for bg in baked_goods_by_price
-    ]
-    return make_response( baked_goods_by_price_serialized, 200  )
-   
+def get_baked_goods_by_price():
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_serialized = [bg.to_dict() for bg in baked_goods]
+    return jsonify(baked_goods_serialized)
 
 @app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
-    most_expensive_serialized = most_expensive.to_dict()
-    return make_response( most_expensive_serialized,   200  )
+def get_most_expensive_baked_good():
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first_or_404()
+    return jsonify(most_expensive.to_dict())
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
